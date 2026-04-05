2250ff155f5173493ff5e9657731b7b8f998594c
Tappy-TechX <tappyblack2@gmail.com>
2026-04-05 16:35:45 +0300

    Sʜᴀᴅᴏᴡ-Xᴛᴇᴄʜ

--- gift/connection/serializer.js
+++ gift/connection/serializer.js
@@ -148,7 +148,7 @@ const serializeMessage = async (ms, Gifted, settings = {}) => {
         command,
         args,
         q: args.join(' '),
-        pushName: ms.pushName || (ms.key.fromMe ? Gifted.user?.name : null) || 'Shadow-Xtech-V1' User',
+        pushName: ms.pushName || (ms.key.fromMe ? Gifted.user?.name : null) || '𝐀𝐓𝐀𝐒𝐒𝐀-𝐌𝐃 User',
         quoted,
         repliedMessage,
         mentionedJid,
